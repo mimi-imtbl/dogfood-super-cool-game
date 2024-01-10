34415c2a8bc29a659e043d0c2e6d12aa77c3167e
Mimi Immutable <mimi.tran@immutable.com>
2024-01-10 16:01:42 +1100

    Add routing to login page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import LandingPage from './pages/LandingPage';
 import GamePage from './pages/GamePage';
 import './App.css';
 import LevelUpPage from './pages/LevelUpPage';
+import Login from './pages/Login';
 
 function App() {
   const isAuthenticated = true;
@@ -22,6 +23,10 @@ function App() {
               element={
                 isAuthenticated ? <Navigate to="/game" /> : <LandingPage />
               }
+            />
+            <Route
+              path="/redirect"
+              element={isAuthenticated ? <Login /> : <Navigate to="/" />}
             />
             <Route
               path="/game"
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -15,5 +15,5 @@ export default function Login() {
     }
   }, [passport]);
 
-  return;
+  return <></>;
 }
