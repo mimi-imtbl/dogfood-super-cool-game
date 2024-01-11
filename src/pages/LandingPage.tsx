e29433454b9c108e84233adf2ac887e0e9f30b39
Jhonatan Gonzalez <jhesgodi@gmail.com>
2024-01-11 00:25:38 -0500

    add lets play btn on landing

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useGameContext } from "../context/GameContext";
-import { Heading, Icon } from "@biom3/react";
+import { Button, Heading, Icon } from "@biom3/react";
 import { Layout } from "../components/Layout";
+import { useNavigate } from "react-router-dom";
 
 interface LandingPageProps {}
 const LandingPage: React.FC<LandingPageProps> = () => {
   const { isAuthenticated, isConnecting } = useGameContext();
+  const navigate = useNavigate();
 
   return (
     <Layout
@@ -22,6 +24,33 @@ const LandingPage: React.FC<LandingPageProps> = () => {
             ? "Please follow the prompts"
             : "Please connect your wallet to play the game"}
         </Heading>
+      )}
+      {isAuthenticated && (
+        <>
+          <Heading sx={{ my: "base.spacing.x4" }}>You are all set!</Heading>
+          <Icon
+            icon="PancakeSwap"
+            sx={{
+              width: "base.icon.size.600",
+              fill: "base.color.accent.1",
+              my: "base.spacing.x4",
+            }}
+          />
+          <Button
+            onClick={() => {
+              navigate("/game");
+            }}
+          >
+            Lets Play!
+            <Button.Icon
+              icon="ArrowForward"
+              sx={{
+                width: "base.icon.size.300",
+                fill: "base.color.accent.1",
+              }}
+            />
+          </Button>
+        </>
       )}
     </Layout>
   );
